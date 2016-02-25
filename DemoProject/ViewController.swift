e4d57bd7f300bbb41a25828e524cbc24be875729
Lynch Wong <lynch.wong@me.com>
2016-02-25 09:49:56 +0800

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Lynch Wong on 2/25/16.
+//  Copyright © 2016 Lynch Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
